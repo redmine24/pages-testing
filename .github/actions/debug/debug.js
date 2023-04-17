2eb53641878323bb1e986c7601fa6d768e9482a9
Dima Nelen <dimanelen@gmail.com>
2023-04-17 21:06:47 +0200

    test 45

--- .github/actions/debug/debug.js
+++ .github/actions/debug/debug.js
@@ -10,7 +10,7 @@ async function run() {
 	const [owner, repo] = core.getInput("repo", { required: true }).split("/")
 	const octokit = new OctoPag({ auth: token })
 
-	const data = await octokit.paginate('GET /repos/'+owner+'/'+repo+'/actions/artifacts', {
+	let data = await octokit.paginate('GET /repos/'+owner+'/'+repo+'/actions/artifacts', {
 	  owner: owner,
 	  repo: repo,
 	  per_page: 100,
