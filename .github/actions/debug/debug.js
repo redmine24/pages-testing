511c57fad8cda1578d4be9b3d37739f5bc955095
Dima Nelen <dimanelen@gmail.com>
2023-04-17 14:15:21 +0200

    test 16

--- .github/actions/debug/debug.js
+++ .github/actions/debug/debug.js
@@ -1,26 +1,29 @@
 const core = require('@actions/core')
 const { Octokit } = require("@octokit/core");
 
-const octokit = new Octokit({
-  auth: process.env.INPUT_TOKEN
-})
 
-try {
+async function run() {
+	try {
 
-	// const artifacts = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
-	const [owner, repo] = process.env.INPUT_REPO.split("/")
-	console.log('owner: ' + owner + ', repo: ' + repo);
+		// const artifacts = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
+		const token = core.getInput("github_token", { required: true })
+		const [owner, repo] = core.getInput("repo", { required: true }).split("/")
+		console.log('owner: ' + owner + ', repo: ' + repo);
 
-	const artifacts = octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
-	  owner: 'OWNER',
-	  repo: 'REPO',
-	  headers: {
-		'X-GitHub-Api-Version': '2022-11-28'
-	  }
-	});
+		const octokit = new Octokit({ auth: process.env.INPUT_TOKEN })
+		const artifacts = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
+		  owner: 'OWNER',
+		  repo: 'REPO',
+		  headers: {
+			'X-GitHub-Api-Version': '2022-11-28'
+		  }
+		});
 
-	console.log(artifacts);
+		console.log(artifacts);
 
-} catch (error) {
-	core.setFailed(error.message);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
+
+run()
