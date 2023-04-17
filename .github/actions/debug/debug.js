5215098ef8cc20186ff110c06db11c5d4333681b
Dima Nelen <dimanelen@gmail.com>
2023-04-17 21:12:26 +0200

    test 46

--- .github/actions/debug/debug.js
+++ .github/actions/debug/debug.js
@@ -2,15 +2,14 @@ const core = require('@actions/core')
 const { Octokit } = require("@octokit/core");
 const { paginateRest } = require('@octokit/plugin-paginate-rest');
 
+const token = core.getInput("github_token", { required: true })
+const [owner, repo] = core.getInput("repo", { required: true }).split("/")
 
 const OctoPag = Octokit.plugin(paginateRest);
+const octokit = new OctoPag({ auth: token })
 
 async function run() {
-	const token = core.getInput("github_token", { required: true })
-	const [owner, repo] = core.getInput("repo", { required: true }).split("/")
-	const octokit = new OctoPag({ auth: token })
-
-	let data = await octokit.paginate('GET /repos/'+owner+'/'+repo+'/actions/artifacts', {
+	const data = await octokit.paginate('GET /repos/'+owner+'/'+repo+'/actions/artifacts', {
 	  owner: owner,
 	  repo: repo,
 	  per_page: 100,
@@ -21,8 +20,9 @@ async function run() {
 	return data;
 }
 
-
-const data = run();
-console.log(data);
-
-	//core.setFailed(error.message);
+try {
+	run().then(data => console.log(data));
+} 
+catch (error) {
+	core.setFailed(error.message);
+}
