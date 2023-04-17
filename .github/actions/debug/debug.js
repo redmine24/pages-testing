5605d5265ebeede24ef9a3e0f3aef1dfc7468c94
Dima Nelen <dimanelen@gmail.com>
2023-04-17 10:08:00 +0200

    debug Mon Apr 17 10:08:00 CEST 2023

--- .github/actions/debug/debug.js
+++ .github/actions/debug/debug.js
@@ -10,7 +10,8 @@ try {
 //// Get the JSON webhook payload for the event that triggered the workflow
 //const payload = JSON.stringify(github.context.payload, undefined, 2)
 //console.log(`The event payload: ${payload}`);
-	const downloadResponse = await artifactClient.downloadAllArtifacts();
+	const downloadResponse = artifactClient.downloadAllArtifacts();
+	//const downloadResponse = await artifactClient.downloadAllArtifacts();
 
 	for (response in downloadResponse) {
 		console.log(response.artifactName);
