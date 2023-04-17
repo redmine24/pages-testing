1b62b7b0cc873677826cd2704a6d093df0b6a4ab
Dima Nelen <dimanelen@gmail.com>
2023-04-17 13:34:20 +0200

    test 2

--- .github/actions/debug/debug.js
+++ .github/actions/debug/debug.js
@@ -1,22 +1,22 @@
-const artifact = require('@actions/artifact');
-const artifactClient = artifact.create();
+const { Octokit } = require("@octokit/core");
+
+const octokit = new Octokit({
+  auth: process.env.INPUT_GITHUB_TOKEN
+})
+
 
 try {
-//// `who-to-greet` input defined in action metadata file
-//const nameToGreet = core.getInput('who-to-greet');
-//console.log(`Hello ${nameToGreet}!`);
-//const time = (new Date()).toTimeString();
-//core.setOutput("time", time);
-//// Get the JSON webhook payload for the event that triggered the workflow
-//const payload = JSON.stringify(github.context.payload, undefined, 2)
-//console.log(`The event payload: ${payload}`);
-	const downloadResponse = artifactClient.downloadAllArtifacts();
-	//const downloadResponse = await artifactClient.downloadAllArtifacts();
 
-	for (response in downloadResponse) {
-		console.log(response.artifactName);
-		console.log(response.downloadPath);
-	}
+	const artifacts = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
+	  owner: 'OWNER',
+	  repo: 'REPO',
+	  headers: {
+		'X-GitHub-Api-Version': '2022-11-28'
+	  }
+	});
+
+	console.log(artifacts);
+
 } catch (error) {
 	core.setFailed(error.message);
 }
