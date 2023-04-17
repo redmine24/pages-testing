77e84cde8e36130228a3ebfb0b6576ec522b3323
Dima Nelen <dimanelen@gmail.com>
2023-04-17 16:54:49 +0200

    test 27

--- .github/actions/debug/debug.js
+++ .github/actions/debug/debug.js
@@ -5,19 +5,19 @@ const { Octokit } = require("@octokit/core");
 async function run() {
 	try {
 
-		// const artifacts = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
 		const token = core.getInput("github_token", { required: true })
 		const [owner, repo] = core.getInput("repo", { required: true }).split("/")
-		const octokit = new Octokit({ auth: process.env.INPUT_TOKEN })
+		const octokit = new Octokit({ auth: token })
 
-		const artifacts = await octokit.paginate({'GET /repos/'+owner+'/'+repo+'/actions/artifacts', {
+		const artifacts = await octokit.paginate('GET /repos/'+owner+'/'+repo+'/actions/artifacts', {
 		  owner: owner,
 		  repo: repo,
 		  per_page: 10,
 		  headers: {
 			'X-GitHub-Api-Version': '2022-11-28'
 		  }
-		}});
+		}
+		);
 
 		console.log(JSON.stringify(artifacts));
 
