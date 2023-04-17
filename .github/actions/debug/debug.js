26c195d2541b1980c82db83c3cbbd57e5418b221
Dima Nelen <dimanelen@gmail.com>
2023-04-17 17:44:22 +0200

    test 33

--- .github/actions/debug/debug.js
+++ .github/actions/debug/debug.js
@@ -6,28 +6,22 @@ const { paginateRest } = require('@octokit/plugin-paginate-rest');
 const OctoPag = Octokit.plugin(paginateRest);
 
 async function run() {
-	try {
+	const token = core.getInput("github_token", { required: true })
+	const [owner, repo] = core.getInput("repo", { required: true }).split("/")
+	const octokit = new OctoPag({ auth: token })
 
-		const token = core.getInput("github_token", { required: true })
-		const [owner, repo] = core.getInput("repo", { required: true }).split("/")
-		const octokit = new OctoPag({ auth: token })
-
-		const artifacts = await octokit.paginate('GET /repos/'+owner+'/'+repo+'/actions/artifacts', {
-		  owner: owner,
-		  repo: repo,
-		  per_page: 100,
-		  headers: {
-			'X-GitHub-Api-Version': '2022-11-28'
-		  }
-		}
-		);
-
-		//console.log(JSON.stringify(artifacts));
-		console.log(artifacts);
-
-	} catch (error) {
-		core.setFailed(error.message);
-	}
+	return artifacts = await octokit.paginate('GET /repos/'+owner+'/'+repo+'/actions/artifacts', {
+	  owner: owner,
+	  repo: repo,
+	  per_page: 100,
+	  headers: {
+		'X-GitHub-Api-Version': '2022-11-28'
+	  }
+	});
 }
 
-run()
+try {
+	console.log(run());
+} catch (error) {
+	core.setFailed(error.message);
+}
