29c1cac5056db56e7cf863b3a427ded4c5e36eaa
Dima Nelen <dimanelen@gmail.com>
2023-04-17 13:46:15 +0200

    test 8

--- .github/actions/debug/debug.js
+++ .github/actions/debug/debug.js
@@ -1,13 +1,14 @@
+const core = require('@actions/core')
 const { Octokit } = require("@octokit/core");
 
 const octokit = new Octokit({
   auth: process.env.INPUT_GITHUB_TOKEN
 })
 
-
 try {
 
 	// const artifacts = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
+	const [owner, repo] = core.getInput("repo", { required: true }).split("/")
 	const artifacts = octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
 	  owner: 'OWNER',
 	  repo: 'REPO',
@@ -17,6 +18,7 @@ try {
 	});
 
 	console.log(artifacts);
+	console.log('owner: ' + owner + ', repo: ' + repo);
 
 } catch (error) {
 	core.setFailed(error.message);
