203882d0d067a14e5f8cd12e2f2b01f88a7231b5
Dima Nelen <dimanelen@gmail.com>
2023-04-17 21:20:48 +0200

    test 47

--- .github/actions/debug/debug.js
+++ .github/actions/debug/debug.js
@@ -9,7 +9,7 @@ const OctoPag = Octokit.plugin(paginateRest);
 const octokit = new OctoPag({ auth: token })
 
 async function run() {
-	const data = await octokit.paginate('GET /repos/'+owner+'/'+repo+'/actions/artifacts', {
+	return data = await octokit.paginate('GET /repos/'+owner+'/'+repo+'/actions/artifacts', {
 	  owner: owner,
 	  repo: repo,
 	  per_page: 100,
@@ -17,7 +17,6 @@ async function run() {
 		'X-GitHub-Api-Version': '2022-11-28'
 	  }
 	});
-	return data;
 }
 
 try {
