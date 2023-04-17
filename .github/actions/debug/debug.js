d45b85c0839881edb2032e3c3f62ab377bb6d3d5
Dima Nelen <dimanelen@gmail.com>
2023-04-17 14:18:22 +0200

    test 19

--- .github/actions/debug/debug.js
+++ .github/actions/debug/debug.js
@@ -11,7 +11,7 @@ async function run() {
 		console.log('owner: ' + owner + ', repo: ' + repo);
 
 		const octokit = new Octokit({ auth: process.env.INPUT_TOKEN })
-		const artifacts = await octokit.request('GET /repos/'+ owner '/' +repo +'/actions/artifacts', {
+		const artifacts = await octokit.request('GET /repos/'+owner+'/'+repo+'/actions/artifacts', {
 		  owner: owner,
 		  repo: repo,
 		  headers: {
