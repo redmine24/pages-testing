1479318c5e5c6874ebccb640e6f6d59db8c6e2bf
Dima Nelen <dimanelen@gmail.com>
2023-04-17 10:05:18 +0200

    debug Mon Apr 17 10:05:18 CEST 2023

--- .github/actions/debug/debug.js
+++ .github/actions/debug/debug.js
@@ -1,9 +1,21 @@
 const artifact = require('@actions/artifact');
 const artifactClient = artifact.create();
-const downloadResponse = await artifactClient.downloadAllArtifacts();
 
-// output result
-for (response in downloadResponse) {
-    console.log(response.artifactName);
-    console.log(response.downloadPath);
+try {
+//// `who-to-greet` input defined in action metadata file
+//const nameToGreet = core.getInput('who-to-greet');
+//console.log(`Hello ${nameToGreet}!`);
+//const time = (new Date()).toTimeString();
+//core.setOutput("time", time);
+//// Get the JSON webhook payload for the event that triggered the workflow
+//const payload = JSON.stringify(github.context.payload, undefined, 2)
+//console.log(`The event payload: ${payload}`);
+	const downloadResponse = await artifactClient.downloadAllArtifacts();
+
+	for (response in downloadResponse) {
+		console.log(response.artifactName);
+		console.log(response.downloadPath);
+	}
+} catch (error) {
+	core.setFailed(error.message);
 }
