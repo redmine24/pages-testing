3b1ac630523f92dbf072b70de133cb17abe6736f
Dima Nelen <dimanelen@gmail.com>
2023-04-29 13:43:03 +0200

    adm-zip debug

--- .github/actions/artifacts-download-by-regex/download.js
+++ .github/actions/artifacts-download-by-regex/download.js
@@ -31,10 +31,11 @@ async function download_artifact(id) {
 	core.info(`download_artifact(${id})`)
 	const artifact = await octokit.request('GET /repos/'+owner+'/'+repo+'/actions/artifacts/'+id+'/zip', { headers: { 'X-GitHub-Api-Version': '2022-11-28' } });
 	const zip = new AdmZip(Buffer.from(artifact.data));
-	zip.getEntries().forEach((e) => { 
-		const b = e.getData();
-		console.log("File: " + e.entryName + ", length (bytes): " + b.length + ", contents: " + b.toString("utf-8"));
-	});
+	zip.extractAllTo(`${path}/${id}`);
+//zip.getEntries().forEach((e) => { 
+//	const b = e.getData();
+//	console.log("File: " + e.entryName + ", length (bytes): " + b.length + ", contents: " + b.toString("utf-8"));
+//});
 }
 
 
